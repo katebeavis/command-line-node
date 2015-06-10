f8bb304a59a03ffec1e1fefba88b62606a8c782d
katebeavis <katebeavis84@gmail.com>
2015-06-10 15:54:28 +0100

    Date is received as a json object

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var username = "katebeavis";
+
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in Javascript";
+  console.log(message);
+};
+
+var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
+  var body = "";
+    response.on('data', function(chunk) {
+    body += chunk;
+  });
+    response.on('end', function() {
+      var profile = JSON.parse(body);
+      console.dir(profile);
+    }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+});
