136698534d82dde86d532bc4a1dbc36b45efc4af
katebeavis <katebeavis84@gmail.com>
2015-06-10 17:24:42 +0100

    Refactored get request into seperate file

---
+++ profile.js
@@ -0,0 +1,33 @@
+var http = require("http");
+
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in Javascript";
+  console.log(message);
+};
+
+function printError(error) {
+  console.log("Got error: " + error.message);
+};
+
+function get(username) {
+  var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
+    var body = "";
+      response.on('data', function(chunk) {
+      body += chunk;
+    });
+    response.on('end', function() {
+      if (response.statusCode === 200) {
+        try {
+          var profile = JSON.parse(body);
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+        } catch(error) {
+          printError(error);
+        }
+      } else {
+        printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+      };
+    });
+  });
+};
+
+module.exports.get = get;
