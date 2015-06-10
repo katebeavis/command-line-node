9b3b11be56b1acdd438be670f19990253ccfcad5
katebeavis <katebeavis84@gmail.com>
2015-06-10 16:55:19 +0100

    Added try and catch error handling

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 };
 
+function printError(error) {
+  console.log("Got error: " + error.message);
+};
+
 var request = http.get("http://teamtreehouse.com/" + username + ".json", function(response) {
   var body = "";
     response.on('data', function(chunk) {
     body += chunk;
   });
-    response.on('end', function() {
-      var profile = JSON.parse(body);
-      printMessage(username, profile.badges.length, profile.points.JavaScript);
-    }).on('error', function(e) {
-    console.log("Got error: " + e.message);
+  response.on('end', function() {
+    if (response.statusCode === 200) {
+      try {
+        var profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        printError(error);
+      }
+    } else {
+      printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+    };
   });
 });
